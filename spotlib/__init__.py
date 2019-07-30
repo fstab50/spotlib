921d799e0e21629f2b80624f40f2f9f8e3ad945b
Blake Huber <blakeca00@gmail.com>
2019-07-30 10:42:17 -0500

    spotlib/__init__.py: exception handling expansion for logging to provide tolerance when installing from pypi.python.org

--- spotlib/__init__.py
+++ spotlib/__init__.py
@@ -1,18 +1,19 @@
 from spotlib._version import __version__ as version
 from spotlib.statics import local_config
 
-try:
-    from libtools import Colors
-    from libtools import logd
-except Exception:
-    pass
-
 
 __author__ = 'Blake Huber'
 __version__ = version
 __email__ = "blakeca00@gmail.com"
 
 
-# global logger
-logd.local_config = local_config
-logger = logd.getLogger(__version__)
+try:
+    from libtools import Colors
+    from libtools import logd
+
+    # global logger
+    logd.local_config = local_config
+    logger = logd.getLogger(__version__)
+
+except Exception:
+    pass
