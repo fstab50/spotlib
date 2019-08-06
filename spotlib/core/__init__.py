73bd59e02addde07ac88758839871a09c477ce1e
Blake Huber <blakeca00@gmail.com>
2019-08-06 18:38:33 -0500

    core/utc.py: new module for correct conversion of dt into utc string values

--- spotlib/core/__init__.py
+++ spotlib/core/__init__.py
@@ -1,6 +1,7 @@
 from spotlib._version import __version__ as version
 from spotlib.core.endpoints import DurationEndpoints
 from spotlib.core.spotcore import EC2SpotPrices
+from spotlib.core.utc import UtcConversion
 
 
 __author__ = 'Blake Huber'
